f118a3692d0a71d7d234daad12ed2682ac8f07a7
Michael Brozhko <Michael@brozhko.org>
2021-01-31 15:52:47 +0000

    haskell start

--- lab1-intro/lisp/tasks.lisp
+++ lab1-intro/lisp/tasks.lisp
@@ -5,9 +5,13 @@
 ;; code for Task 1.2(b):
 (format t "type something: ")
 (setf input (read-line))
-(format t "you typed: ~a" input)
+(if (string= input "")
+    (format t "nothing typed")
+    (format t "you typed: ~a" input))
 
 ;; code for Task 1.2(c):
 (setf prg '(+ 1 n)) ; define a very simple program
-(print prg) ; print the program
+;;(print prg) ; print the program
 ; TODO: execute the program with n = 1 and print its result
+(setf n 1)
+(print(eval prg))
